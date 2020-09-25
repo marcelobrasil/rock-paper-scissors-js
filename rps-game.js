59a02110424763bf4f5929b540575b6afc3beb2a
marcelobrasil <marcelosbrsl@gmail.com>
2020-09-25 12:10:28 +0100

    don't remember what I've changed actually

--- rps-game.js
+++ rps-game.js
@@ -1,56 +1,68 @@
 /* Function declarations */
+function startGame(e){
+    const container = e.target.parentElement;
+
+    /* removes game start button */
+    container.removeChild(e.target);
+
+    /* creates and adds player selection buttons */
+    const rockButton = document.createElement('button')
+    const paperButton = document.createElement('button')
+    const scissorsButton = document.createElement('button')
+    rockButton.setAttribute('id','rock-button');
+    rockButton.setAttribute('class','button');
+    rockButton.textContent = 'Rock';
+    paperButton.setAttribute('id','paper-button');
+    paperButton.setAttribute('class','button');
+    paperButton.textContent = 'Paper';
+    scissorsButton.setAttribute('id','scissors-button');
+    scissorsButton.setAttribute('class','button');
+    scissorsButton.textContent = 'Scissors';
+    container.appendChild(rockButton);
+    container.appendChild(paperButton);
+    container.appendChild(scissorsButton);
+
+    /*  */
+    rockButton.addEventListener('click', play);
+    paperButton.addEventListener('click', play);
+    scissorsButton.addEventListener('click', play);
+};
 
 function computerPlay() {
-    const play = (Math.random() * 100);
-    if (play < 33) {
-        return 'rock'
-    } else if (33 <= play < 66) {
-        return 'paper'
-    } else {return 'scissors'}
+    const value = Math.random() * 100;
+    if (value < 33) {return 'rock'}
+    else if (33 <= value < 66) {return 'paper'}
+    else {return 'scissors'};
 };
 
-function playRound(playerSelection) {
-    playerSelection.toLowerCase();
-    const computerSelection = computerPlay()
-    if (playerSelection === 'rock'){
-        if (computerSelection === 'rock'){
-            return "It's a tie!"
-        } else if (computerSelection === 'paper'){
-            return "You lose! paper beats rock."
-        } else {return "You win! rock beats scissors."};
-    } else if (playerSelection === 'paper'){
-        if (computerSelection === 'rock'){
-            return "You win! paper beats rock."
-        } else if (computerSelection === 'paper'){
-            return "It's a tie!"
-        } else {return "You lose! scissors beat paper."};
-    } else {
-        if (computerSelection === 'rock'){
-            return "You lose! rock beats scissors"
-        } else if (computerSelection === 'paper'){
-            return "You win! scissors beat paper."
-        } else {return "It's a tie!"};
-    };
-};
-
-function game() {
-    let playerCounter = 0;
-    let computerCounter = 0;
-    for (let i = 0; i < 6; i++) {
-        let playerPlay = prompt();
-        let result = playRound(playerPlay);
-        if (result.includes('win')) {
-            playerCounter++
-        } else if (result.includes('lose')) {
-            computerCounter++
-        };
-        console.log(result);
+function play(e){
+    messageBoard = document.getElementById('message');
+    choice = e.target.textContent;
+    choice.toLowerCase;
+    computerChoice = computerPlay();
+    if (choice === 'Rock') {
+        if (computerChoice === 'rock') {
+            messageBoard.textContent = "It's a tie!";
+        } else if (computerChoice === 'paper') {
+            messageBoard.textContent = "You lose! the computer chose paper.";
+        } else if (computerChoice === 'scissors'){messageBoard.textContent = "You win!";};
     }
-    if (playerCounter > computerCounter) {
-        console.log("You're the winner! You scored " + playerCounter + " against " + computerCounter);
-    } else if (computerCounter > playerCounter) {
-        console.log("You're the loser! You scored " + playerCounter + " against " + computerCounter);
-    } else {console.log("This game was a tie! Both you and the computer scored " + playerCounter)}
-};
+    else if (choice === 'Paper') {
+        if (computerChoice === 'paper') {
+            messageBoard.textContent = "It's a tie!";
+        } else if (computerChoice === 'scissors') {
+            messageBoard.textContent = "You lose! the computer chose scissors.";
+        } else if (computerChoice === 'rock'){messageBoard.textContent = "You win!";};
+    }
+    else if (choice === 'Scissors'){
+        if (computerChoice === 'scissors') {
+            messageBoard.textContent = "It's a tie!";
+        } else if (computerChoice === 'rock') {
+            messageBoard.textContent = "You lose! the computer chose rock.";
+        } else  if (computerChoice === 'paper'){messageBoard.textContent = "You win!";};
+    }
+}
 
-game()
+/* Game */
+const playButton = document.getElementById('game-start');
+playButton.addEventListener('click', startGame);
